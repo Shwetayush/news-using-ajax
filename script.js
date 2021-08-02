9f980e314b9e0cf04280f5864340fd81eaf7a6d4
Shwetayush <45384473+Shwetayush@users.noreply.github.com>
2021-08-02 22:53:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function remove_image(){
+function remove_image() {
   document.getElementById('img').remove();
 }
+const closeBtn = document.getElementById('close-btn');
+closeBtn.addEventListener('click', () => {
+  document.getElementById('img').remove();
+  document.getElementById('collapse0').remove();
+});
 let source = 'bbc-news';
 let apiKey = '5c118d2ce51e43c8b4146412a18a7283'
 let newsAccordion = document.getElementById('newsAccordion');
@@ -9,18 +14,18 @@ const xhr = new XMLHttpRequest();
 xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
 
 xhr.onload = function () {
-    if (this.status === 200) {
-        let json = JSON.parse(this.responseText);
-        let articles = json.articles;
-        console.log(articles);
-        let newsHtml = "";
-        articles.forEach(function(element, index) {
-            let news = `<div class="card">
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    console.log(articles);
+    let newsHtml = "";
+    articles.forEach(function (element, index) {
+      let news = `<div class="card">
                             <div class="card-header" id="heading${index}">
                                 <h2 class="mb-0">
                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
                                     aria-expanded="false" aria-controls="collapse${index}">
-                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                   <b>Breaking News ${index + 1}:</b> ${element["title"]}
                                 </button>
                                 </h2>
                             </div>
@@ -29,13 +34,13 @@ xhr.onload = function () {
                                 <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
                             </div>
                         </div>`;
-            newsHtml += news;
-        });
-        newsAccordion.innerHTML = newsHtml;
-    }
-    else {
-        console.log("Some error occured")
-    }
+      newsHtml += news;
+    });
+    newsAccordion.innerHTML = newsHtml;
+  }
+  else {
+    console.log("Some error occured")
+  }
 }
 
 xhr.send()
